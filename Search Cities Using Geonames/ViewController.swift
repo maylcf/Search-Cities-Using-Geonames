6ba28849096fa0ff3268ff1ab93cc4968aa6eefb
Mayara Felix <mayaralcabral@gmail.com>
2017-12-08 11:58:13 -0500

    Initial Commit

---
+++ Search Cities Using Geonames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search Cities Using Geonames
+//
+//  Created by Mayara Felix on 2017-12-08.
+//  Copyright © 2017 maylcf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
